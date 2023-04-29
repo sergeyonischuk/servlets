0c8f98afc48d08f24cb6ea56f54ef0ddf2a9dfd7
sergeyonischuk <36tXg8Le%>
2023-04-29 22:13:49 +0300

    thymeleaf added

--- src/main/java/ThymeleafTestController.java
+++ src/main/java/ThymeleafTestController.java
@@ -3,8 +3,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
@@ -13,13 +11,14 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@WebServlet("/time")
-public class TimeServlet extends HttpServlet {
+@WebServlet("/test-template")
+public class ThymeleafTestController extends HttpServlet {
     private TemplateEngine engine;
 
     @Override
-    public void init() {
+    public void init() throws ServletException {
         engine = new TemplateEngine();
+
         FileTemplateResolver resolver = new FileTemplateResolver();
         resolver.setPrefix("./templates/");
         resolver.setSuffix(".html");
@@ -47,31 +46,5 @@ public class TimeServlet extends HttpServlet {
 
         engine.process("test", simpleContext, resp.getWriter());
         resp.getWriter().close();
-    }
-
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html; charset=utf-8");
-        String timezone = req.getParameter("timezone");
-        resp.getWriter().write(getTimeAndUTC(timezone));
-        resp.getWriter().close();
-    }
-
-    public String getTimeAndUTC(String timezone) {
-        DateTime dateTime = new DateTime().withZone(DateTimeZone.forID("GMT"));
-        dateTime = getDateWithUtc(getUTCDigit(timezone), dateTime);
-        return dateTime.toString("yyyy-MM-dd HH:mm:ss") + " " + timezone;
-    }
-
-    private int getUTCDigit(String utcParameter) {
-        if (utcParameter.contains("-")) {
-            return Integer.parseInt(utcParameter.substring((utcParameter.indexOf("-"))));
-        } if (utcParameter.contains("+")) {
-            return Integer.parseInt(utcParameter.substring((utcParameter.indexOf(utcParameter.substring(4)))));
-        }
-        else return 0;
-    }
-
-    private DateTime getDateWithUtc(int utc, DateTime dateTime) {
-            return dateTime.plusHours(utc);
     }
 }
